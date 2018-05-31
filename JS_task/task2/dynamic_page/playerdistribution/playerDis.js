cdf27265aa41111df9e7280d35db4ffe0c96fcc9
huguie <37169366+huguie@users.noreply.github.com>
2018-05-31 10:40:03 +0800

    任务2

--- JS_task/task2/dynamic_page/playerdistribution/playerDis.js
+++ JS_task/task2/dynamic_page/playerdistribution/playerDis.js
@@ -1,18 +1,19 @@
 let word = document.getElementById('changeWord');//输入栏value
 let killerNum = document.getElementById('killerNum');//杀手人数
 let civilianNum = document.getElementById('civilianNum');//平民人数
-let  x = document.getElementById("myRange");//获取滑条
+let x = document.getElementById("myRange");//获取滑条
 let less = document.getElementById('less');
 let plus = document.getElementById('plus');
 let returm = document.getElementById('return');
 let pageAlert = document.getElementById('pageAlert');
-
+let setBtn = $('#setBtn');
+let player = [];
+let playerNum = $('#playerNum');
 
 function isNum(s){//是否为正整数
     let re = /^[0-9]+$/ ;
     return re.test(s)
 }
-
 
 function inputWord(){
     if(isNum(word.value) === false){
@@ -54,11 +55,11 @@ function rangeWord(){
     console.log();
     };//按钮事件，点击一次，滑条value增加，关联输入栏value
 
- less.onclick = function (){ 
+ less.onclick = function (){
     x.value--;
     word.value = x.value;
-     killerNum.innerHTML = Math.floor(word.value/3);
-     civilianNum.innerHTML = Math.ceil(word.value * 2 / 3);
+    killerNum.innerHTML = Math.floor(word.value/3);
+    civilianNum.innerHTML = Math.ceil(word.value * 2 / 3);
     console.log();
     };//按钮事件，点击一次，滑条value减少，关联输入栏value
 pageAlert.onclick = function(){
@@ -69,26 +70,53 @@ returm.onclick = function(){
 };
 
 
+
+setBtn.on('click',function () {
+if (player.length !== 0){
+    // for (let i = 0; i < player.length; i++){
+    //     allPlayer.remove();}
+    $('.player').remove();}
+
+player.length = 0;
+    for (let i = 0; i < parseInt(killerNum.innerHTML); i++){
+    player.push('杀  &nbsp;手 1人');}//获取动态杀手数量
+
+for (let i = 0; i < parseInt(civilianNum.innerHTML); i++){
+    player.push('平  &nbsp;民 1人');}//获取动态动态平民数量
+
+let w = player.length;
+let z,y;
+for( ; w ; ){
+    y = Math.floor(Math.random()*(w--));
+    z = player[w];
+    player[w] = player[y];
+    player[y] = z;}
+
+for (let i = 0; i < player.length; i++){
+allPlayer = $('<div class="player">\n' +
+    '<div class="whitebox">'  +'</div>\n' +
+    '    <div>' + player[i] + '</div>\n' +
+    '</div>\n');
+
+playerNum
+    .append(allPlayer);}
+});//洗牌算法，将数组打乱
+
+
+
 document.getElementById("viewId").onclick = function(){
+
     sessionStorage.setItem("wordStorage",word.value);
     sessionStorage.setItem('killerNum',killerNum.innerHTML);
     sessionStorage.setItem('civilianNum',civilianNum.innerHTML);
-    // if(isNum(word.value) === false){
-    // alert('不是整数');
-    // sessionStorage.clear();
-    //     word.value = '0';
-    //     killerNum.innerHTML = '0';
-    //     civilianNum.innerHTML = '0';
-    // }
-    // else{
-    // if  (word.value <= 18 && word.value >= 4){
     window.location.href="../viewId/view.html";
-    // }
-    // else {
-    //     alert('请输入4-18以内的数字 ');
-    //     word.value = '0';
-    //     killerNum.innerHTML = '0';
-    //     civilianNum.innerHTML = '0';
-    // }}
 };//点击跳转事件
+
+
+
+
+
+
+
+
 
